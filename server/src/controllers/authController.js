b221cbc1bc5d91b6472a6565223375292d02413e
Nishita Kumari <nishitamishra1207@gmail.com>
2026-05-23 15:58:02 +0530

    added password hashing and email validation

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,16 +1,32 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const registerUser = async (req, res) => {
      const {name, email, password } = req.body;
 
+     //check existing user 
+     const existingUser = await User.findOne({ email });
+
+     if(existingUser) {
+      return res.status(400).json({
+         meassage:"User already exists",
+      });
+     }
+
+     //Hash Password
+     const hashedPassword = await bcrypt.hash(password, 10);
+
+     //Create new User
      const user = new User({
         name,
         email,
-        password,
+        password: hashedPassword,
      });
 
+     //Save user in database
      await user.save();
 
+     //success response 
      res.status(201).json({
         message: "User registered successfully",
      });
